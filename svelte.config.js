217a05bce544b077ddaab37dc8d1a0987f7259a4
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-07-29 04:42:37 +0200

    Added mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,29 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  extensions: [".svelte", ...mdsvexConfig.extensions],
 
-	kit: {
-		prerender: {
-			crawl: true,
-			enabled: true,
-			onError: 'continue',
-			default: true
-		},
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
-	}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [preprocess(), mdsvex(mdsvexConfig)],
+
+  kit: {
+    prerender: {
+      crawl: true,
+      enabled: true,
+      onError: "continue",
+      default: true,
+    },
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: null,
+    }),
+  },
 };
 
 export default config;
