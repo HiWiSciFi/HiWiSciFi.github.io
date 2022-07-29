fa27566304edf97ee1877fb9d87cc9360b1cdee6
Max Hager <19999080+HiWiSciFi@users.noreply.github.com>
2022-07-29 04:25:04 +0200

    Updates

---
+++ _app/immutable/pages/__layout.svelte-9712bca7.js
@@ -0,0 +1,49 @@
+import{F as _t,S as ft,i as dt,s as mt,l as v,a as y,G as I,u as F,m as f,p as o,h as a,c as w,H as M,v as G,q as t,I as O,r as z,b as R,J as r,n as ut,K as gt,L as bt,x as xt,y as Et,z as Ct,M as qt,N as $t,O as yt,f as ht,t as vt,C as wt}from"../chunks/index-06ce5a74.js";const Lt=()=>{const l=_t("__svelte__");return{page:{subscribe:l.page.subscribe},navigating:{subscribe:l.navigating.subscribe},get preloading(){return console.error("stores.preloading is deprecated; use stores.navigating instead"),{subscribe:l.navigating.subscribe}},session:l.session,updated:l.updated}},Ht={subscribe(l){return Lt().page.subscribe(l)}};function At(l){let s,p,d,i,c,_,k,u,b,x,C,S,n,e,m,T,g,q,K,U,L,V,Y,H,A,h,Z,B,j,Q,P,X;return{c(){s=v("header"),p=v("div"),d=y(),i=v("nav"),c=I("svg"),_=I("path"),k=y(),u=v("ul"),b=v("li"),x=v("a"),C=F("Home"),S=y(),n=v("li"),e=v("a"),m=F("About"),T=y(),g=v("li"),q=v("a"),K=F("Projects"),U=y(),L=I("svg"),V=I("path"),Y=y(),H=v("div"),A=v("a"),h=I("svg"),Z=I("path"),B=I("path"),j=I("path"),Q=y(),P=v("style"),X=F(`.github-corner:hover .octo-arm {\r
+				animation:octocat-wave 560ms ease-in-out\r
+			}\r
+			\r
+			@keyframes octocat-wave {\r
+				0%, 100% {\r
+					transform:rotate(0)\r
+				}\r
+				20%,60% {\r
+					transform:rotate(-25deg)\r
+				}\r
+				40%,80% {\r
+					transform:rotate(10deg)\r
+				}\r
+			}\r
+			\r
+			@media (max-width:500px) {\r
+				.github-corner:hover .octo-arm {\r
+					animation:none\r
+				}\r
+				\r
+				.github-corner .octo-arm {\r
+					animation:octocat-wave 560ms ease-in-out\r
+				}\r
+			}`),this.h()},l($){s=f($,"HEADER",{class:!0});var E=o(s);p=f(E,"DIV",{class:!0});var pt=o(p);pt.forEach(a),d=w(E),i=f(E,"NAV",{class:!0});var D=o(i);c=M(D,"svg",{class:!0,viewBox:!0,"aria-hidden":!0});var tt=o(c);_=M(tt,"path",{d:!0,class:!0}),o(_).forEach(a),tt.forEach(a),k=w(D),u=f(D,"UL",{class:!0});var N=o(u);b=f(N,"LI",{class:!0});var et=o(b);x=f(et,"A",{"sveltekit:prefetch":!0,href:!0,class:!0});var rt=o(x);C=G(rt,"Home"),rt.forEach(a),et.forEach(a),S=w(N),n=f(N,"LI",{class:!0});var at=o(n);e=f(at,"A",{href:!0,class:!0});var st=o(e);m=G(st,"About"),st.forEach(a),at.forEach(a),T=w(N),g=f(N,"LI",{class:!0});var ot=o(g);q=f(ot,"A",{href:!0,class:!0});var nt=o(q);K=G(nt,"Projects"),nt.forEach(a),ot.forEach(a),N.forEach(a),U=w(D),L=M(D,"svg",{class:!0,viewBox:!0,"aria-hidden":!0});var lt=o(L);V=M(lt,"path",{d:!0,class:!0}),o(V).forEach(a),lt.forEach(a),D.forEach(a),Y=w(E),H=f(E,"DIV",{class:!0});var W=o(H);A=f(W,"A",{href:!0,class:!0,"aria-label":!0});var it=o(A);h=M(it,"svg",{width:!0,height:!0,viewBox:!0,style:!0,"aria-hidden":!0});var J=o(h);Z=M(J,"path",{d:!0,class:!0}),o(Z).forEach(a),B=M(J,"path",{d:!0,fill:!0,style:!0,class:!0}),o(B).forEach(a),j=M(J,"path",{d:!0,fill:!0,class:!0}),o(j).forEach(a),J.forEach(a),it.forEach(a),Q=w(W),P=f(W,"STYLE",{});var ct=o(P);X=G(ct,`.github-corner:hover .octo-arm {\r
+				animation:octocat-wave 560ms ease-in-out\r
+			}\r
+			\r
+			@keyframes octocat-wave {\r
+				0%, 100% {\r
+					transform:rotate(0)\r
+				}\r
+				20%,60% {\r
+					transform:rotate(-25deg)\r
+				}\r
+				40%,80% {\r
+					transform:rotate(10deg)\r
+				}\r
+			}\r
+			\r
+			@media (max-width:500px) {\r
+				.github-corner:hover .octo-arm {\r
+					animation:none\r
+				}\r
+				\r
+				.github-corner .octo-arm {\r
+					animation:octocat-wave 560ms ease-in-out\r
+				}\r
+			}`),ct.forEach(a),W.forEach(a),E.forEach(a),this.h()},h(){t(p,"class","corner svelte-1rxqui9"),t(_,"d","M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z"),t(_,"class","svelte-1rxqui9"),t(c,"class","cornerBox svelte-1rxqui9"),t(c,"viewBox","0 0 2 3"),t(c,"aria-hidden","true"),t(x,"sveltekit:prefetch",""),t(x,"href","/"),t(x,"class","svelte-1rxqui9"),t(b,"class","svelte-1rxqui9"),O(b,"active",l[0].url.pathname==="/"),t(e,"href","/about"),t(e,"class","svelte-1rxqui9"),t(n,"class","svelte-1rxqui9"),O(n,"active",l[0].url.pathname==="/about"),t(q,"href","/projects"),t(q,"class","svelte-1rxqui9"),t(g,"class","svelte-1rxqui9"),O(g,"active",l[0].url.pathname==="/projects"),t(u,"class","svelte-1rxqui9"),t(V,"d","M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z"),t(V,"class","svelte-1rxqui9"),t(L,"class","cornerBox svelte-1rxqui9"),t(L,"viewBox","0 0 2 3"),t(L,"aria-hidden","true"),t(i,"class","svelte-1rxqui9"),t(Z,"d","M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"),t(Z,"class","svelte-1rxqui9"),t(B,"d","M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"),t(B,"fill","currentColor"),z(B,"transform-origin","130px 106px"),t(B,"class","octo-arm svelte-1rxqui9"),t(j,"d","M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"),t(j,"fill","currentColor"),t(j,"class","octo-body svelte-1rxqui9"),t(h,"width","80"),t(h,"height","80"),t(h,"viewBox","0 0 250 250"),z(h,"fill","#000000"),z(h,"color","#ffffff"),z(h,"position","absolute"),z(h,"top","0"),z(h,"border","0"),z(h,"right","0"),t(h,"aria-hidden","true"),t(A,"href","https://github.com/HiWiSciFi"),t(A,"class","github-corner svelte-1rxqui9"),t(A,"aria-label","View source on GitHub"),t(H,"class","corner svelte-1rxqui9"),t(s,"class","svelte-1rxqui9")},m($,E){R($,s,E),r(s,p),r(s,d),r(s,i),r(i,c),r(c,_),r(i,k),r(i,u),r(u,b),r(b,x),r(x,C),r(u,S),r(u,n),r(n,e),r(e,m),r(u,T),r(u,g),r(g,q),r(q,K),r(i,U),r(i,L),r(L,V),r(s,Y),r(s,H),r(H,A),r(A,h),r(h,Z),r(h,B),r(h,j),r(H,Q),r(H,P),r(P,X)},p($,[E]){E&1&&O(b,"active",$[0].url.pathname==="/"),E&1&&O(n,"active",$[0].url.pathname==="/about"),E&1&&O(g,"active",$[0].url.pathname==="/projects")},i:ut,o:ut,d($){$&&a(s)}}}function Bt(l,s,p){let d;return gt(l,Ht,i=>p(0,d=i)),[d]}class It extends ft{constructor(s){super(),dt(this,s,Bt,At,mt,{})}}function Mt(l){let s,p,d,i,c,_,k,u,b,x,C;s=new It({});const S=l[1].default,n=bt(S,l,l[0],null);return{c(){xt(s.$$.fragment),p=y(),d=v("main"),n&&n.c(),i=y(),c=v("footer"),_=v("p"),k=F("This page is still in development. Check out my "),u=v("a"),b=F("GitHub"),x=F(" in the meantime!"),this.h()},l(e){Et(s.$$.fragment,e),p=w(e),d=f(e,"MAIN",{class:!0});var m=o(d);n&&n.l(m),m.forEach(a),i=w(e),c=f(e,"FOOTER",{class:!0});var T=o(c);_=f(T,"P",{});var g=o(_);k=G(g,"This page is still in development. Check out my "),u=f(g,"A",{href:!0,class:!0});var q=o(u);b=G(q,"GitHub"),q.forEach(a),x=G(g," in the meantime!"),g.forEach(a),T.forEach(a),this.h()},h(){t(d,"class","svelte-1izrdc8"),t(u,"href","https://github.com/HiWiSciFi"),t(u,"class","svelte-1izrdc8"),t(c,"class","svelte-1izrdc8")},m(e,m){Ct(s,e,m),R(e,p,m),R(e,d,m),n&&n.m(d,null),R(e,i,m),R(e,c,m),r(c,_),r(_,k),r(_,u),r(u,b),r(_,x),C=!0},p(e,[m]){n&&n.p&&(!C||m&1)&&qt(n,S,e,e[0],C?yt(S,e[0],m,null):$t(e[0]),null)},i(e){C||(ht(s.$$.fragment,e),ht(n,e),C=!0)},o(e){vt(s.$$.fragment,e),vt(n,e),C=!1},d(e){wt(s,e),e&&a(p),e&&a(d),n&&n.d(e),e&&a(i),e&&a(c)}}}function kt(l,s,p){let{$$slots:d={},$$scope:i}=s;return l.$$set=c=>{"$$scope"in c&&p(0,i=c.$$scope)},[i,d]}class jt extends ft{constructor(s){super(),dt(this,s,kt,Mt,mt,{})}}export{jt as default};
---
+++ _app/immutable/pages/about.svelte-ae791ed7.js
@@ -0,0 +1,1 @@
+import{S as e,i as n,s as o}from"../chunks/index-06ce5a74.js";function a(t){return[]}class u extends e{constructor(s){super(),n(this,s,a,null,o,{})}}export{u as default};
